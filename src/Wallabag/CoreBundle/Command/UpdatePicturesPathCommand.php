d3f099afb77ba31944521c4a98645241145d3a44
Nicolas Lœuillet <nicolas.loeuillet@prestashop.com>
2023-06-29 17:21:16 +0200

    php cs

--- src/Wallabag/CoreBundle/Command/UpdatePicturesPathCommand.php
+++ src/Wallabag/CoreBundle/Command/UpdatePicturesPathCommand.php
@@ -3,7 +3,6 @@
 namespace Wallabag\CoreBundle\Command;
 
 use Doctrine\ORM\EntityManagerInterface;
-use JMS\Serializer\SerializerBuilder;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
